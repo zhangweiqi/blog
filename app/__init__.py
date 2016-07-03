bb540b2b530b8e08b03e3f4e372be6f01f2715d4
张炜奇 <zhangweiqi1015@gmail.com>
2016-07-03 17:45:50 +0800

    7.2

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
+from flask_pagedown import PageDown
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
 mail = Mail()
+moment= Moment()
+pagedown=PageDown()
+
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'  # 'auth.login' is login_page blueprint route
@@ -24,6 +29,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    moment.init_app(app)
+    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
